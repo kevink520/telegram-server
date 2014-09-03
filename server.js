73ee5c73fbe75b879f893aa1aeb23f7adfb772f0
Kevin <kevinkim75@gmail.com>
2014-09-02 19:39:40 -0700

    Implemented persistent authentication session and cleaned up for readability.

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ var users = {
 var posts = {
   'posts': [
     {
-		  'id': '1',
+      'id': '1',
       'author': 'fastcompany',
       'body': 'Leica celebrates 100 years with a georgeously minimalist shooter that pays homage to its first <a href="http://wrd.cm/1ieFplL">http://wrd.cm/1ieFplL</a> <a href="http://pic.twitter.com/SNvy9PGZwc">pic.twitter.com/SNvy9PGZwc</a>',
       'createdDate': new Date(2014, 3, 24, 13, 39, 0).toISOString()    
@@ -44,7 +44,7 @@ var posts = {
       'author': 'fastcompany',
       'body': 'This app is like a remote control for your credit cards: <a href="http://f-st.co/OXzg2Ew">http://f-st.co/OXzg2Ew</a> <a href="http://pic.twitter.com/eAL1sdVhrh">pic.twitter.com/eAL1sdVhrh</a>',
       'createdDate': new Date(2014, 3, 24, 13, 40, 0).toISOString()
-	  },
+    },
     {
       'id': '3',
       'author': 'clarkewolfe',
@@ -56,13 +56,13 @@ var posts = {
       'author': 'johnmaeda',
       'body': 'Great teams constantly learn and re-learn how to move from the ego of *I* to the ego of *WE*.',
       'createdDate': new Date(2014, 3, 24, 13, 45, 0).toISOString()
-	  },
+    },
     {
-	 	  'id': '5',
+      'id': '5',
       'author': 'cristianstrat',
       'body': 'Ponies enhance cognition. To be safe around horses you must stay focused, alert, aware, and never forget that they can kick your ass.',
       'createdDate': new Date(2014, 3, 24, 13, 50, 0).toISOString()
-	  }
+    }
   ]
 };
 
